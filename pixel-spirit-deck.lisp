b05b754e42127e02330d0d34bb5c35aa30da7102
azimut <azimut.github@protonmail.com>
2018-05-09 10:04:26 -0300

    start using single stage pipeline

--- pixel-spirit-deck.lisp
+++ pixel-spirit-deck.lisp
@@ -1,26 +1,26 @@
 (in-package :pixel-spirit-deck)
 
-(defun-g vert ((vert :vec2))
-  (values (v! vert 0 1)
-          (+ (v2! .5) (* vert .5))))
-
 ;;; 000-void.frag
-(defun-g frag ((uv :vec2) &uniform (resolution :vec2)
+(defun-g frag ((uv :vec2)
+               &uniform (resolution :vec2)
                (time :float))
   (v! 0 0 0 1))
 
-(defpipeline-g pipe ()
-  (vert :vec2)
-  (frag :vec2))
+(defpipeline-g pipe (:points)
+  :fragment (frag :vec2))
+
+(defparameter *bs*
+  (make-buffer-stream nil :primitive :points))
 
 (defun draw! ()
   (let ((res (surface-resolution (current-surface))))
     (setf (viewport-resolution (current-viewport))
           res)
     (as-frame
-      (map-g #'pipe (get-quad-stream-v2)
+      (map-g #'pipe *bs*
              :resolution res
-             :time (/ (get-internal-real-time) 1000f0)))))
+             :time (/ (get-internal-real-time)
+                      1000f0)))))
 
 (def-simple-main-loop play ()
   (draw!))
