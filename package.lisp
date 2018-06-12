7ad8412a06776085e523a24e06524f16d825cbb4
azimut <azimut.github@protonmail.com>
2018-06-12 14:18:31 -0300

    export all the things

--- package.lisp
+++ package.lisp
@@ -1,3 +1,26 @@
 (uiop:define-package pixel-spirit-deck
     (:use #:cl #:cepl #:rtg-math #:vari #:nineveh
-          :livesupport))
+          :livesupport)
+  (:export #:defcard ;; macro to define fragment "frag" shader
+           ;; misc
+           #:aastep
+           #:bridge
+           #:flip
+           #:g-fill
+           #:rotate
+           #:scale
+           #:stroke
+           ;; sdf's
+           #:circle-sdf
+           #:cross-sdf
+           #:flower-sdf
+           #:heart-sdf
+           #:hex-sdf
+           #:poly-sdf
+           #:rays-sdf
+           #:rect-sdf
+           #:rhomb-sdf
+           #:spiral-sdf
+           #:star-sdf
+           #:tri-sdf
+           #:vesica-sdf))
